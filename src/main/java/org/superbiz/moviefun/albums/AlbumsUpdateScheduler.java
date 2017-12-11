5d2202d28f2d7a75c842409ef1665d2cfcf1a9ae
Ryan Tracey <rtracey9123@gmail.com>
2017-12-11 10:05:37 -0500

    Background jobs with DB

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -1,11 +1,15 @@
 package org.superbiz.moviefun.albums;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -17,22 +21,47 @@ public class AlbumsUpdateScheduler {
 
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final JdbcTemplate jdbcTemplate;
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource dataSource) {
         this.albumsUpdater = albumsUpdater;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
     @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
     public void run() {
         try {
-            logger.debug("Starting albums update");
-            albumsUpdater.update();
+            if (startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+                albumsUpdater.update();
+                logger.debug("Finished albums update");
 
-            logger.debug("Finished albums update");
+            } else {
+                logger.debug("Nothing to start");
+            }
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private static DataSource buildDataSource(String url, String username, String password) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    private Boolean startAlbumSchedulerTask() {
+       int updatedRows = jdbcTemplate.update(
+               "UPDATE album_scheduler_task" +
+                       " SET started_at = now()" +
+                       " WHERE started_at IS NULL" +
+                       " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+       );
+
+        return updatedRows > 0;
     }
 }
